443a98c13b3e66194dccf50a31617d3fe1d74a1a
papesoukad96 <papesoukad@gmail.com>
2023-03-15 17:19:42 +0100

    ffggg

---
+++ back_end/index.js
@@ -0,0 +1,146 @@
+const express = require('express');/* recupere la variable express dans la boite express */
+const mongoose  = require('mongoose'); //gere link api base de donnees
+require('dotenv').config();/* pour recuperer le fichier env */
+var MongoClient = require('mongodb').MongoClient;
+var cors = require('cors') //configuration des differentes requettes pour acceder aux ressources
+
+const routes = require('./routes/routes');
+
+const databaseLink = process.env.DATABASE_URL/* permet de recuperer le lien de la base de donnée */
+
+mongoose.connect(databaseLink);/* permet d'avoir access à la base mongodb */
+const database = mongoose.connection
+
+const app = express(); /* express sert a ecouté les ports et à envoyer des données */
+
+app.use(cors({origin:'*'}));/*   */
+
+app.use(express.json());/* affiche les fichiers au format json */
+
+app.use('/api', routes);
+
+database.on('error', (error)=> {
+
+console.log(error)
+
+})
+
+//Creation server socket 
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
+database.once('connected', ()=> {
+    
+console.log('Database Connected')
+
+})
+
+var fs = require('fs');
+/* var index = fs.readFileSync( '/'); */
+
+const { SerialPort } = require('serialport');
+var { ReadlineParser } = require("@serialport/parser-readline")
+const router = require('./routes/routes');
+/* const { Socket } = require('socket.io'); */
+/* const parsers = SerialPort.parsers; */
+/* var path = require('path') */
+
+
+
+
+/* var port = new SerialPort({ path:'/dev/ttyUSB0',
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});  */
+
+/* var parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' })); */
+
+/* port.pipe(parser); */
+var url = "mongodb+srv://MamySy:mamy@cluster0.qwexmvm.mongodb.net/";
+
+
+var temoin = '0'
+
+io.on('connection', function(socket) {
+    
+    console.log('Node is listening to port');
+    socket.on("active", (arg) => {
+        console.log(arg); // world
+        temoin = arg;
+      });
+    
+});
+
+
+
+/* parser.on('data', function(data) { */
+    
+    //console.log('les information sont: ' + data);
+    /* temp = data.split('/');
+    var temperature = data.slice(0, 2); //decoupe de la temperature
+    var humidite = data.slice(3, 5); //decoupe de l'humidite
+    //console.log(data.split('/'));
+    io.emit('data', {"temperature": temperature, "humidite": humidite});
+    var datHeure = new Date(); */
+   /*  var min = datHeure.getMinutes();
+    var heur = datHeure.getHours(); //heure
+    var sec = datHeure.getSeconds(); //secondes
+    var mois = datHeure.getDate(); //renvoie le chiffre du jour du mois 
+    var numMois = datHeure.getMonth() + 1; //le mois en chiffre
+    var laDate = datHeure.getFullYear(); // me renvoie en chiffre l'annee
+    if (numMois < 10) { numMois = '0' + numMois; }
+    if (mois < 10) { mois = '0' + mois; }
+    if (sec < 10) { sec = '0' + sec; }
+    if (min < 10) { min = '0' + min; }
+    var heureInsertion = heur + ':' + min + ':' + sec;
+    var heureEtDate = mois + '/' + numMois + '/' + laDate; */
+    //console.log(heureInsertion);
+    //console.log(heureEtDate);
+    const fetchMovies = (socket) => {
+        data.findAll()
+            .then(data => io.emit('fetchMovies', data))
+            .catch(logError)
+    }
+    /* var temperature = data.slice(0, 2); //decoupe de la temperature
+    var humidite = data.slice(3, 5); //decoupe de l'humidite */
+  /*   var tempEtHum = { "temperature": temperature, "humidite": humidite,'Date': heureEtDate, 'Heure': heureInsertion }; */
+  /*   if ((heur == 17 && min == 10 && sec == 00) || (heur == 12 && min == 00 && sec == 00) || (heur == 19 && min == 00 && sec == 00)) { */
+    //if(sec == 00){ 
+         //Connexion a mongodb et insertion Temperature et humidite
+    /*      MongoClient.connect(url, { useUnifiedTopology: false }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("test");
+            dbo.collection("tempHum2").insertOne(tempEtHum, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            });
+        })
+    } //Fin if
+}
+    
+/* ); */
+
+
+
+  http.listen(3001, ()=>{
+    console.log('server started at ${3001}')/* apres avoir ecouter le port 3000 affiche les données */
+})
+/* parser.on('mute', function(mute){
+MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("dhtTemp2");
+    var col = dbo.collection('tempHum2');
+    col.find().toArray(function(err, items) {
+        console.log(items);
+        io.emit('mute', items);     
+console.log(items);
+        
+})
+
+})
+} ) */
---
+++ back_end/routes/routes.js
@@ -0,0 +1,211 @@
+const express = require('express');
+const Model = require('../models/userModel');
+const Modeltemp = require('../models/userModel copy');
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const check = require('./midleware');
+var MongoClient = require('mongodb').MongoClient;
+const router = express.Router();
+var url = "mongodb+srv://MamySy:mamy@cluster0.qwexmvm.mongodb.net/";
+module.exports = router;
+
+/* pour la connection */
+router.post("/login",  async (req, res, next) => {
+
+    let { email, password } = req.body;
+    
+    let existingUser;
+    
+    existingUser = await Model.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(400).send("email doesn't exist...!");
+    }else if(existingUser.etat == false){
+      return res.status(401).send("user est archivé...!");
+    }
+      
+      
+    //check if password is correct
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordValid) {
+      return res.status(400).send("password is invalid");
+    }
+    
+    
+    let token;
+    try {
+      //Creating jwt token
+      token = jwt.sign(
+        { userId: existingUser.id, email: existingUser.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      console.log(err);
+      const error = new Error("Erreur! Quelque chose s'est mal passée.");
+      return next(error);
+    }
+    
+    res
+      .status(200)
+      .json({
+        success: true,
+        data: {
+          userId: existingUser.id,
+          email: existingUser.email,
+          roles: existingUser.roles,
+          img: existingUser.img,
+          token: token,
+        },
+    });
+});
+
+/*  la méthode POST passe les paramètres dans le corps de la requête. */
+router.post('/post', async(req, res) => {
+
+const { email, password, prenom, nom, date_inscri, roles, etat, matricule, img } = req.body;
+const users = [];
+
+const newUser = Model({
+    email,
+    password, 
+    prenom, 
+    nom, 
+    date_inscri,
+    roles,
+    etat, 
+    matricule,
+    img
+
+});
+
+try {
+
+  const oldUser = await Model.findOne({ email });
+
+  if (oldUser) {
+    return res.status(409).send("Email Already Exist. Please Login");
+  }
+
+    const hash = await bcrypt.hash(newUser.password,10);
+    newUser.password = hash;
+
+    users.push(newUser);
+    /* res.json(newUser); */
+    await newUser.save();
+
+    res.status(201).json(newUser);
+
+} catch(error) {
+    res.status(400).json({message: error.message})
+}
+
+})
+/* La méthode GET est utilisée par le navigateur pour 
+demander au serveur de renvoyer une certaine ressource. */
+router.get('/getAll',check, async(req, res) => {
+try{
+const data = await Model.find();
+res.json(data)
+}
+catch(error){
+res.status(500).json({message: error.message})
+}
+})
+/* get by id methode */
+router.get('/getOne/:id', async(req, res) => {
+const data = await Model.findById(req.params.id);
+res.json(data)
+})
+
+/* update by id methode  pour la mdodification*/
+router.patch('/update/:id', async (req, res) => {
+try {
+const id = req.params.id;
+const updatedData = req.body;
+const options = { new: true };
+
+if (updatedData.password){
+    const hash = await bcrypt.hash(updatedData.password, 10);
+    updatedData.password = hash;
+    
+            const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+            );
+    
+          return  res.send(result);
+    
+        }
+
+
+    const result = await Model.findByIdAndUpdate(
+        id, updatedData, options
+    )
+
+   return res.send(result)
+}
+catch (error) {
+    res.status(400).json({ message: error.message })
+}
+})
+/* delete by id method pour supprimer */
+
+router.delete('/delete/:id', async(req, res) => {
+try {
+const id = req.params.id;
+const data = await Model.findByIdAndDelete(id)
+res.send(`Le Document avec le nom ${data.prenom} ${data.nom} a été supprimé..`)
+}
+catch (error) {
+res.status(400).json({ message: error.message })
+}
+})
+
+/* get all method */
+
+router.get('/pap', async(req, res) => {
+  try{
+  /* const data = await Modeltemp.find();
+  console.log(data);
+  res.json(data) */
+
+  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    var col = dbo.collection('tempHum2');
+    col.find().toArray(function(err, items) {
+        console.log(items);
+             res.json(items)
+console.log(items);
+
+})
+
+})
+  }
+  catch(error){
+  res.status(500).json({message: error.message})
+  }
+  })
+  // 
+  router.get('/deletetemp', async(req, res) => {
+    try {
+      MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("test");
+        var col = dbo.collection('tempHum2');
+        col.deleteMany()
+            
+    })
+    
+    
+    }
+    catch (error) {
+    res.status(400).json({ message: error.message })
+    }
+    })
+  // list data
+/* router.get('/pap', function(req, res) {
+  Modeltemp.find(function (err, sales) {
+      if (err) return next(err);
+      res.json(sales);
+  });
+}); */
