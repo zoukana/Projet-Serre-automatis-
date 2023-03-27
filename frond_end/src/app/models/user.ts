e007a7de15c85772ff3876607656933b08bf14b7
oumytoure <toureoumy12@gmail.com>
2023-03-27 11:45:46 +0000

    test

--- frond_end/src/app/models/user.ts
+++ frond_end/src/app/models/user.ts
@@ -1,18 +1,8 @@
 export interface User {
-/* nom?: String;
-prenom?: String; */
 email?: String;
-/* roles?: String;
-date_inscri?: String;
-etat?: boolean;
-matricule?: String;
-date_modif?:Date;
-date_archive?:Date; */
 password?: String;
 data?:{
     token: String;
-    /* roles: String;
-    img: String; */
     email:String;
 };
 }
