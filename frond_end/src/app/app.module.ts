1d02f04e35c7ca61a7e0ce76ad7704ee930ab7bb
papesoukad96 <papesoukad@gmail.com>
2023-03-27 10:14:30 +0200

    mise a jour 27/03

--- frond_end/src/app/app.module.ts
+++ frond_end/src/app/app.module.ts
@@ -6,16 +6,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './users/header/header.component';
-import { UtilisateurComponent } from './users/utilisateur/utilisateur.component';
-import { ModificationComponent } from './modification/modification.component';
-import { SimpleusersComponent } from "./users/simpleusers/simpleusers.component";
+
 import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { LoginComponent } from './login/login.component';
-import { InscrptionComponent } from './inscrption/inscrption.component';
-import { PageArchiveComponent } from './page-archive/page-archive.component';
-import { ProfilComponent } from './profil/profil.component';
+
 import { AccueilDashboardComponent } from './accueil-dashboard/accueil-dashboard.component';
 import { SidebarComponent } from './users/sidebar/sidebar.component';
 import { TableHistoriqueComponent } from './table-historique/table-historique.component';
@@ -33,14 +28,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3001', options: {
 @NgModule({
     declarations: [
         AppComponent,
-        HeaderComponent,
-        UtilisateurComponent,
-        ModificationComponent,
-        SimpleusersComponent,
-        LoginComponent,
-        InscrptionComponent,
-        PageArchiveComponent,
-        ProfilComponent,
+       LoginComponent,
         AccueilDashboardComponent,
         SidebarComponent,
         TableHistoriqueComponent,
