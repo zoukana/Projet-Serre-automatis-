c20f1654a53de50ddbae0fea86bde294584fd07c
koumba6 <diongue901@gmail.com>
2023-03-28 14:47:24 +0000

    cc

--- frond_end/src/app/login/login.component.ts
+++ frond_end/src/app/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
     
-      email:['',[Validators.required,Validators.email]],
+      email:['',[Validators.required,Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       
       password:['',[Validators.required,Validators.minLength(8)]],
       
