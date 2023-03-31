c14efc8cf809acf44f2dc959a51c078f557a4cf3
oumytoure <toureoumy12@gmail.com>
2023-03-31 08:50:53 +0000

    test

--- frond_end/src/app/app.component.ts
+++ frond_end/src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
   showHead:any;
   history: Set<any> = new Set()
   hist: any[] = []
-  currentUser: any = {};
+ 
 
    prenom = localStorage.getItem('prenom');   
     nom = localStorage.getItem('nom');
