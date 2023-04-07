e7ba21a6339e340d49279230a7f8ec6811d28895
papesoukad96 <papesoukad@gmail.com>
2023-04-07 17:45:17 +0200

    xxxc

--- frond_end/src/app/services/users.service.ts
+++ frond_end/src/app/services/users.service.ts
@@ -26,7 +26,7 @@ export class UsersService {
   }
  getConnexion(user:User){
     return this.httpClient.post<User>(`${env.apiUrl}/login`,user).
-      pipe(map(res => {
+      pipe(map(user => {
         // store user details and jwt token in local storage to keep user logged in between page refreshes
        /*  console.log(user.data)  */
         localStorage.setItem('currentUser', JSON.stringify(user.data?.token));
